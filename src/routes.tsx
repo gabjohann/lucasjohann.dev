589bb0fefb5d2079256b8c35cd173058f8ca5d52
Lucas Gabriel <gabif.johann@gmail.com>
2024-01-13 17:18:14 -0300

    docs: add vercel deploy settings

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,21 +7,6 @@ import { Technologies } from './pages/Technologies'
 import { About } from './pages/About'
 import { Experience } from './pages/Experience'
 
-/* export function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projetos" element={<Projects />} />
-        <Route path="/artigos" element={<Articles />} />
-        <Route path="/tecnologias" element={<Technologies />} />
-        <Route path="/experiencias" element={<Experience />} />
-        <Route path="/sobre" element={<About />} />
-      </Route>
-    </Routes>
-  )
-} */
-
 export const router = createBrowserRouter([
   {
     path: '/',

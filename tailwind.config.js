55405cc5b0a87641c49f15f829fce6917ea27320
Lucas Gabriel <gabif.johann@gmail.com>
2024-09-07 20:39:55 -0300

    chore: extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        baloo: ['Baloo 2', 'cursive'],
+        roboto: ['Roboto', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
+      },
+      backgroundImage: {
+        noise: "url('/bg-noise.png')",
+      },
+      dropShadow: {
+        custom: '0 1px 1px rgb(255 255 255 / 1)',
+      },
+    },
   },
   plugins: [],
 }

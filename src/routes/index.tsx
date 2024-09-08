71214a9c5f4b6055a9b2a2feddad1e7816f9eb97
Lucas Gabriel <gabif.johann@gmail.com>
2024-09-08 01:19:28 -0300

    feat: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { App } from '../app'
+import { About } from './about'
+import { Home } from './home'
+import { Experience } from './experience'
+import { ErrorPage } from './errorPage'
+import { Technologies } from './technologies'
+import { Certificates } from './certificates'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/experience',
+        element: <Experience />,
+      },
+      {
+        path: '/technologies',
+        element: <Technologies />,
+      },
+      {
+        path: '/certificates',
+        element: <Certificates />,
+      },
+    ],
+  },
+])
+
+export default router

6bd63aa044c48f961d76225e9249a387ebdff57a
Lucas Gabriel <gabif.johann@gmail.com>
2024-12-23 17:13:34 -0300

    refactor: remove class

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ import { Header } from './components/header'
 
 export function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex flex-grow items-center justify-center pt-[84px]">
         <Outlet />
